c60e40da71193bef0eccb95b418ca4ff34e09d2d
Ivonne Klauser <ivonne.klauser@siemens.com>
2020-08-08 19:28:54 +0200

    fix: correct varibales in insert statements

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -8,7 +8,7 @@ class LoadFactOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 redshift_conn_id='redshift',
+                 redshift_conn_id="redshift",
                  sql_statement = "",
                  target_table = "",
                  *args, **kwargs):
@@ -22,5 +22,5 @@ class LoadFactOperator(BaseOperator):
         self.log.info('LoadFactOperator start with table {}'.format(self.target_table))
         self.hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
         redshift_hook = PostgresHook(self.redshift_conn_id)        
-        redshift_hook.run(self.sql_stat)
+        redshift_hook.run(self.sql_statement)
         self.log.info('LoadFactOperator finished with table {}'.format(self.target_table))
